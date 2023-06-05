dd377f733c3867b79a3ccb2406f4e28141d4f59f
Timothee20V <timothee.viard@reseau.eseo.fr>
2023-06-05 14:27:52 +0200

    3.6.3

---
+++ src/fr/eseo/e3/poo/projet/blox/modele/BloxException.java
@@ -0,0 +1,18 @@
+package fr.eseo.e3.poo.projet.blox.modele;
+
+public class BloxException extends Exception{
+
+    public static final int BLOX_COLLISION = 0;
+    public static final int BLOX_SORTIE_PUITS = 0;
+
+    private int type;
+
+    public BloxException(String message, int type){
+        super(message);
+        this.type = type;
+    }
+
+    public int getType(){
+        return this.type;
+    }
+}
